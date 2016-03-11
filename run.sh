f23ccca04456c71807f5a7266bbec7d5901a99a4
Andre Abrantes <contato@andreabrantes.com>
2016-03-11 20:10:59 -0300

    fancy run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ echo Iniciando testes...
 
 ./teste.sh > tempo.txt
 
-echo Testes realizados com sucesso!
+echo Testes realizados com sucesso em apenas $(cat tempo.txt | awk '{ sum += $4 } END { print sum/60 " minutos" }')!
 
 exit 0;
