862b16ef9eb59ddc54d425c53e952a40cf2ca00e
Andre Abrantes <contato@andreabrantes.com>
2016-03-11 19:46:30 -0300

    melhorando os scripts. Monitor inicia e finaliza automaticamente

---
+++ run.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+# Script para medição do projeto de ADSD - UFCG
+# TODO cabecalho
+
+echo Iniciando testes...
+
+./teste.sh > tempo.txt
+
+echo Testes realizados com sucesso!
+
+exit 0;
